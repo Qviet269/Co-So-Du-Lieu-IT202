ea2fe4719c6c6926416282a4892e7fdcdbd822be
Quốc Việt <vietq984@gmail.com>
2026-05-06 13:03:32 +0700

    new

--- session5/baitap3.sql
+++ session5/baitap3.sql
@@ -9,3 +9,6 @@ where status = 'AVAILABLE'
 and trust_score > 80
 order by distance_km asc,
 		trust_score desc;
+	
+-- Nếu min_trust_score = -10, điều kiện trust_score >= -10 sẽ luôn đúng (vì trust_score thường từ 0-100). 
+-- Hệ thống sẽ lấy hết tài xế AVAILABLE kể cả những người rất tệ.
